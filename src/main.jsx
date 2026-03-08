fe4cecac11612dd671adf235590e3a61ccb40bed
rayesc1 <raymond.escalona1@gmail.com>
2026-03-07 23:30:52 -0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Numb3r5 from './Numb3r5.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Numb3r5 />
+  </StrictMode>
+)
